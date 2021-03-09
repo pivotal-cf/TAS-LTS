115147d927b479485d30a5978c9cbbf021b5b452
John Frederick Cornish IV <jcornish@pivotal.io>
2021-03-09 12:43:36 -0700

    Update wiki and add string replacements to build script

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@
 #cp roam-export/*.md TAS-LTS.wiki
 
 for f in roam-export/*.md ; do
-  processed_f_name=$(basename "${f// /-}")
-  cp "${f}" "TAS-LTS.wiki/$processed_f_name"
+  name_with_spaces_replaced=$(basename "${f// /-}")
+  processed_file_name="${name_with_spaces_replaced/Curriculum---/}"
+  cp "${f}" "TAS-LTS.wiki/$processed_file_name"
 done
 
 # This would be nice but doesn't matter
 # Fix Roam Research TODOs
-#for f in TAS-LTS.wiki/Curriculum*.md ; do
-#  sed -i 's/\{\{\[\[TODO\]\]\}\}/\[ \]/g' "${f}"
-#done
+for f in TAS-LTS.wiki/*.md ; do
+  sed -i '' -e 's/Curriculum - //g' "${f}"
+  sed -i '' -e 's/{{\[\[TODO\]\]}}/\[ \]/g' "${f}"
+done
